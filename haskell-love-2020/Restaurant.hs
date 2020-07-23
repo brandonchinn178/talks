f8ffdc2913dcc751f4d9f5b725aace2e29f1054f
Brandon Chinn <brandon@leapyear.io>
2020-07-22 20:14:07 -0700

    Finish type-level programming section

--- haskell-love-2020/Restaurant.hs
+++ haskell-love-2020/Restaurant.hs
@@ -3,11 +3,14 @@
 
 data Status = OPEN | CLOSED
 
-data Restaurant (status :: Status) = Restaurant { name :: String }
+data Restaurant (status :: Status) = Restaurant String
   deriving (Show)
 
 createRestaurant :: String -> Restaurant 'OPEN
 createRestaurant name = Restaurant name
+
+getName :: Restaurant status -> String
+getName (Restaurant name) = name
 
 closeRestaurant :: Restaurant 'OPEN -> Restaurant 'CLOSED
 closeRestaurant (Restaurant name) = Restaurant name
